06f5575273e2f818339d26a0b42ffb71353ff892
Mitchell Shelton <history@osu.edu>
2012-11-14 09:07:42 -0500

    Moved all files up one directory.

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ */
+?>
+
+</div>
+
+<div class="footer">
+	<p>This is the footer that will appear on every page</p>
+</div>
+
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The home page for Catco
+ *
+ * By: Alex Heck
+ *
+ */
+require($_SERVER['DOCUMENT_ROOT']."catco/includes/header.php");
+require($_SERVER['DOCUMENT_ROOT']."catco/includes/footer.php");
+
+?>
